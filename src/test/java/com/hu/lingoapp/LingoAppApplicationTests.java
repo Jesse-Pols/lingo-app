fe2d7cd33da39acd28521f0ee0d44c6be025782b
Jesse <jessepols@gmail.com>
2020-11-23 20:20:06 +0100

    project setup

---
+++ src/test/java/com/hu/lingoapp/LingoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hu.lingoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LingoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
